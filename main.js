85f18c0f31bcd7161bd876f68d7f8ca7b141ad89
irwanphan <irwanphan@gmail.com>
2021-05-25 15:20:56 +0700

    function splitted, found bug: isComplete value is not set yet

--- main.js
+++ main.js
@@ -1,65 +1,59 @@
 const storageKey = "bookEntry";
+const submitButton = document.getElementById("bookSubmit") 
 
 function checkForStorage() {
     return typeof(Storage) !== "undefined"
 }
-
-const submitButton = document.getElementById("bookSubmit") 
-
-submitButton.addEventListener('click', function() {
+function getBookList() {
     if(checkForStorage()){
         if (localStorage.getItem(storageKey) === null) {
             bookData = [];
         } else {
             bookData = JSON.parse(localStorage.getItem(storageKey));
         }
-        
-        const inputBookTitle = document.getElementById("inputBookTitle").value;
-        const inputBookAuthor = document.getElementById("inputBookAuthor").value;
-        const inputBookYear = document.getElementById("inputBookYear").value;
-        const inputBookIsComplete = document.getElementById("inputBookIsComplete").value;
-        const newBookData = {
-            title: inputBookTitle,
-            author: inputBookAuthor,
-            year: inputBookYear,
-            isComplete: inputBookIsComplete,
-        }
-
-        bookData.unshift(newBookData); 
-
-        localStorage.setItem(storageKey, JSON.stringify(bookData))
-
-        
+    } else {
+        alert("The browser does not support Web Storage")
     }
+}
+
+function addNewBook() {
+    const inputBookTitle = document.getElementById("inputBookTitle").value;
+    const inputBookAuthor = document.getElementById("inputBookAuthor").value;
+    const inputBookYear = document.getElementById("inputBookYear").value;
+    const inputBookIsComplete = document.getElementById("inputBookIsComplete").value;
+    const newBookData = {
+        title: inputBookTitle,
+        author: inputBookAuthor,
+        year: inputBookYear,
+        isComplete: inputBookIsComplete,
+    }
+    bookData.unshift(newBookData); 
+    localStorage.setItem(storageKey, JSON.stringify(bookData))
+}
+
+submitButton.addEventListener('click', function() {
+    getBookList();
+    addNewBook();
 });
 
-function getBookList() {
-    if (checkForStorage()) {
-        return JSON.parse(localStorage.getItem(storageKey)) || [];
-    } else {
-        return [];
+function renderBookShelf(shelf) {
+    shelf.innerHTML = "";
+
+    for (let book of bookData) {
+        let row = document.createElement('article');
+        row.innerHTML += "<h3>" + book.title + "</h3>";
+        row.innerHTML += "<p>Penulis: " + book.author + "</p>";
+        row.innerHTML += "<p>Tahun: " + book.year + "</p>";
+        row.innerHTML += "<div>" + book.isComplate + "</div>";
+
+        shelf.appendChild(row);
     }
 }
 
 window.addEventListener("load", function(){
-    if (checkForStorage) {
-        if (localStorage.getItem(storageKey) !== null){
-            const bookData = getBookList();
-            const incompleteBookshelfList = document.querySelector("#incompleteBookshelfList");
-    
-            incompleteBookshelfList.innerHTML = "";
-        
-            for (let book of bookData) {
-                let row = document.createElement('article');
-                row.innerHTML += "<h3>" + book.title + "</h3>";
-                row.innerHTML += "<p>Penulis: " + book.author + "</p>";
-                row.innerHTML += "<p>Tahun: " + book.year + "</p>";
-                row.innerHTML += "<div>" + book.isComplate + "</div>";
-        
-                incompleteBookshelfList.appendChild(row);
-            }
-        }
-    }else{
-        alert("Browser yang Anda gunakan tidak mendukung Web Storage")
-    }
+    getBookList();
+
+    const incompleteBookshelfList = document.querySelector("#incompleteBookshelfList");
+
+    renderBookShelf(incompleteBookshelfList);
 });
